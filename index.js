a06d361ce6ed67efebd2ec14dd9e80437fb2a3c8
Himalaya <yugmani@yahoo.com>
2020-08-22 00:29:47 -0700

    gitignore;

--- index.js
+++ index.js
@@ -36,52 +36,13 @@ inquirer.prompt([
     if(err) {
         return console.log(err);
     }
-    else console.log("success");
+    else {
+        console.log("success");
+        fs.readFile(filename, "utf-8", (err, data) => {
+            if (err) throw err;
+            console.log(data);
+          });;
+    }
 });
 });
 
-
-// var inquirer = require("inquirer");
-// var fs = require('fs');
-
-// inquirer.prompt([
-//   {
-//     type: "input",
-//     name: "name",
-//     message: "What is your name?"
-//   },
-//   {
-//     type: "checkbox",
-//     message: "What languages do you know?",
-//     name: "stack",
-//     choices: [
-//       "HTML", 
-//       "CSS", 
-//       "JavaScript", 
-//       "MySQL"
-//     ]
-//   },
-//   {
-//     type: "list",
-//     message: "What is your preferred method of communication?",
-//     name: "contact",
-//     choices: [
-//       "email",
-//       "phone",
-//       "telekinesis"
-//     ]
-//   }
-// ]).then(function(data) {
-
-//   var filename = data.name.toLowerCase().split(' ').join('') + ".json";
-
-//   fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
-
-//     if (err) {
-//       return console.log(err);
-//     }
-
-//     console.log("Success!");
-
-//   });
-// });
